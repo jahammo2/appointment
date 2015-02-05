aa023d2c7ce908844f7c8c99992cc2370c8f520c
jahammo2 <jahammo2@gmail.com>
2015-02-05 13:29:40 -0500

    Added the whole date function

--- form-appointment/js/main.js
+++ form-appointment/js/main.js
@@ -2,8 +2,9 @@ $(function() {
 	var wrapper = $('.wrapper');
 	var $editAppointment = $('#edit-appointment');
 	var $backButton = $('.back-button');
+	var $body = $('body');
 	// var $this = $(this)
-	// wrapper.html($("#main-screen").html());
+	wrapper.html($("#new-appointment").html());
 	// wrapper.html($("#deats").html());
 
 	wrapper.on('click', '.new-appt-button', function() {
@@ -13,7 +14,40 @@ $(function() {
 
 	wrapper.on('click', '.back-button', function() {
 		wrapper.html($('#main-screen').html());
-		$blackOverlay.hide();
+		// $blackOverlay.css({
+		// 	'visibility': 'hidden'
+		// })
+		// $deatsWrapper.css({
+		// 	'visibility': 'hidden',
+		// 	'height': '15%',
+		// 	'width': '20%',
+		// 	'top': '30%',
+		// 	'right': '38%',				
+		// 	'transition': '.1s'
+		// })	
+	})
+
+	$('.back-button').on('click', function () {
+		wrapper.html($('#new-appointment').html());
+
+		$blackOverlay.css({
+			'visibility': 'hidden'
+		})
+
+		$deatsWrapper.css({
+			'visibility': 'hidden',
+			'height': '30%',
+			'width': '15%',
+			'top': '40%',
+			'right': '43%',				
+			'transition': '0s'
+		})
+
+		$deatsDiv.css({
+			'height': '30%',
+			'transition': '0s'
+		})
+
 	})
 
 	wrapper.on('click', '.cancel-button', function(e) {
@@ -25,37 +59,74 @@ $(function() {
 	wrapper.on('click', '.submit-button', function(e) {
 		e.stopPropagation();
 		e.preventDefault();
+
+		var dateLabel = $('.appt-date-label-input');
+		var dateLabelValue = dateLabel.val();
+		var newDate = moment(dateLabelValue, ['M/D/YY', 'MM/D/YY', 'MM/DD/YY','M/D/YYYY', 
+			'MM/D/YYYY', 'MM/DD/YYYY']);			
+		var newFormattedFullDate = newDate.format('MMMM D')
+		var newDateFromNow = newDate.fromNow()
+		dateLabel.val(newFormattedFullDate);
+		console.log(newDateFromNow);
+		$('.deats-date-time-span').html(newDateFromNow + ' on');
+		$('.deats-full-date-time-span').html(newFormattedFullDate);
+		var dateInMs = newDate.format('x')
+
 		wrapper.html($('#main-screen').html());
 	})
 
-
 	var $blackOverlay = $('.black-overlay')
-	$blackOverlay.hide();
 	var $deatsWrapper = $('.deats-wrapper')
-	$('.appointment-list-item').on('click', function() {
-		$blackOverlay.show();
+	var $deatsDiv = $('.deats-div')
+
+	// $blackOverlay.hide();
+	// $deatsWrapper.hide();
+
+	wrapper.on('click', '.appointment-list-item', function() {
+		// $blackOverlay.show();
+		// $deatsWrapper.show();
 		// wrapper.html($('#deats').html());
 		
-		// $blackOverlay.css({
-		// 	'visibility': 'visible'
+		$blackOverlay.css({
+			'visibility': 'visible'
 
-		// })
-		
+		})
+
 		$deatsWrapper.css({
 			'visibility': 'visible',
-			'height': '60%',
-			'width': '80%',
-			'top': '20%',
-			'right': '10%',
+			'height': '59%',
+			'width': '30%',
+			'top': '30%',
+			'right': '35%',
 			'transition': '.1s'
 		})
+
+		$deatsDiv.css({
+			'height': '70%',
+			'transition': '.1s'
+		})		
+
 		$blackOverlay.on('click', function() {
+
 			$blackOverlay.css({
 				'visibility': 'hidden'
 			})
+
 			$deatsWrapper.css({
-				'visibility': 'hidden'
+				'visibility': 'hidden',
+				'height': '30%',
+				'width': '15%',
+				'top': '40%',
+				'right': '43%',				
+				'transition': '.1s'
 			})
+
+			$deatsDiv.css({
+				'height': '30%',
+				'transition': '0s'
+			})
+			// $blackOverlay.hide();
+			// $deatsWrapper.hide();		
 		});
 	})
 
@@ -64,15 +135,47 @@ $(function() {
 		e.stopPropagation();
 		e.preventDefault();
 		wrapper.html($('#edit-appointment').html());
-		$blackOverlay.hide();
+		$blackOverlay.css({
+			'visibility': 'hidden'
+		})
+		$deatsWrapper.css({
+			'visibility': 'hidden',
+			'height': '15%',
+			'width': '20%',
+			'top': '30%',
+			'right': '38%',				
+			'transition': '.1s'
+		})	
+	})
+
+	$('.deats-edit-icon').on('click', function (e) {
+		e.stopPropagation();
+		e.preventDefault();
+		wrapper.html($('#edit-appointment').html());
+
+		$blackOverlay.css({
+			'visibility': 'hidden'
+		})
+
+		$deatsWrapper.css({
+			'visibility': 'hidden',
+			'height': '30%',
+			'width': '15%',
+			'top': '40%',
+			'right': '43%',				
+			'transition': '0s'
+		})
+
+		$deatsDiv.css({
+			'height': '30%',
+			'transition': '0s'
+		})
+
 	})
 
 	$('.main-trash').on('click', function () {
 		$(this).closest('li').remove();
 	})
-
-
-
 
 
 })
